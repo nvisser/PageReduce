59eed2758dd00080243922bc94644a6fe67d382d
Niek Visser <niek@bcome.nl>
2017-12-17 18:16:52 +0100

    Add Customer and Order class

--- src/main/java/nl/bcome/pageranker/Join/models/Customer.java
+++ src/main/java/nl/bcome/pageranker/Join/models/Customer.java
@@ -1,4 +1,14 @@
 package nl.bcome.pageranker.Join.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Customer {
+    @SerializedName("CustomerID")
+    int id;
+    @SerializedName("CustomerName")
+    String name;
+    @SerializedName("ContactName")
+    String contactName;
+    @SerializedName("Country")
+    String country;
 }
--- src/main/java/nl/bcome/pageranker/Join/models/Order.java
+++ src/main/java/nl/bcome/pageranker/Join/models/Order.java
@@ -1,4 +1,12 @@
 package nl.bcome.pageranker.Join.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Order {
+    @SerializedName("OrderID")
+    int id;
+    @SerializedName("CustomerID")
+    int customerId;
+    @SerializedName("OrderDate")
+    String date;
 }
