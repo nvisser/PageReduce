f38284d1ff880668c9f9a4e44b52740e4693a962
Niek Visser <niek@bcome.nl>
2017-12-17 18:17:03 +0100

    Add json parsing to the mapper

--- src/main/java/nl/bcome/pageranker/Join/JoinMapper.java
+++ src/main/java/nl/bcome/pageranker/Join/JoinMapper.java
@@ -1,5 +1,8 @@
 package nl.bcome.pageranker.Join;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
+import nl.bcome.pageranker.Join.models.Customer;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -10,10 +13,7 @@ import java.io.IOException;
 class JoinMapper extends Mapper<LongWritable, Text, Text, Text> {
 
     public void map(LongWritable Key, Text value, Context context) throws IOException, InterruptedException {
-        System.out.println("MAP: Key=" + Key.toString() + "\t- Value="+ value.toString());
-//        String[] tokens = value.toString().split("\\s");
-//        for (String s : tokens) {
-            context.write(new Text("key"), new Text("value"));
-//        }
+        Customer c = new GsonBuilder().create().fromJson(value.toString(), Customer.class);
+        context.write(new Text("key"), new Text("value"));
     }
 }
