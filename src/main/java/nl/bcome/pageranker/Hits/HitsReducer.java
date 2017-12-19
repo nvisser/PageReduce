41cc4d745f887adf402908c7f3cae24777684444
Niek Visser <niek@bcome.nl>
2017-12-19 13:50:58 +0100

    Add files for HITS

---
+++ src/main/java/nl/bcome/pageranker/Hits/HitsReducer.java
@@ -0,0 +1,17 @@
+package nl.bcome.pageranker.Hits;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+class HitsReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable i : values) {
+            sum += i.get();
+        }
+        context.write(key, new IntWritable(sum));
+    }
+}
